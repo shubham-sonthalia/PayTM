d6373ea160b85548dc688aafd8707319439fe121
Shubham Sonthalia <shubhamsonthalia@Shubhams-MacBook-Air.local>
2024-03-24 22:35:38 +0530

    updating db name

---
+++ paytm-monorepo/apps/bank-webhook/src/index.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import db from "@repo/db/client";
+
+const app = express();
+// TODO: add zod validation here
+// check if this request acutally cam efrom hdfc bank, use a webhook secret here
+app.post("/", async (req, res) => {
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: Number(paymentInformation.userId),
+        },
+        data: {
+          amount: {
+            // You can also get this from your DB
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+    res.json({
+      message: "Captured",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
+});
+
+// balances
+// onRampTransactions
